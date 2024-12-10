bb0ba3d8e5607e490d22a4a98d738210efa7c7db
uluj <gungoruluc@gmail.com>
2024-12-10 10:04:49 +0300

    added to notes

---
+++ Assets/Notes/SceneNotes1.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class SceneNote1 : MonoBehaviour
+{
+    [TextArea]
+    public string note = "Don't remove things from this scene unless everyone agrees to and everyone needs to \"fetch origin\" after removing from \"MainScene\"";
+}
